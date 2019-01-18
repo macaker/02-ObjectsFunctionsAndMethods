4920dee37bf25a7f6d1f11b1b432436eb29bf7ce
macaker <macaker@rose-hulman.edu>
2019-01-18 14:12:29 -0500

    second class

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -52,7 +52,7 @@ PThrm(54,78)
 
 
 ###############################################################################
-# TODO: 4a.  Define a function immediately below this _TODO_.
+# DONE: 4a.  Define a function immediately below this _TODO_.
 #   It takes two arguments:
 #     -- a string that represents a color (e.g. 'red')
 #     -- a positive integer that represents the thickness of a Pen.
@@ -74,7 +74,7 @@ PThrm(54,78)
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 4b.  In main, CALL your function at least TWICE (with different values
+# DONE: 4b.  In main, CALL your function at least TWICE (with different values
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
