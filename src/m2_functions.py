79e381514b0006f72667e62336af9c25e63b164c
macaker <macaker@rose-hulman.edu>
2019-01-18 14:12:51 -0500

    second class

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -96,7 +96,7 @@ turtles(15, 'yellow')
 ########## You can do it! I believe in you!! Love Eddie :)
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
